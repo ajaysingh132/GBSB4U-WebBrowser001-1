f697bad68f01678b8822d1c37610a3c1dc00a0e9
ajaysingh132 <banjarajanmat@gmail.com>
2025-12-17 07:09:18 +0530

    Update MainActivity.java

--- app/src/main/java/com/gbsb4u/webbrowser/MainActivity.java
+++ app/src/main/java/com/gbsb4u/webbrowser/MainActivity.java
@@ -1,1 +1,26 @@
+package com.gbsb4u.webbrowser;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://gbsb.in");
+    }
+}
